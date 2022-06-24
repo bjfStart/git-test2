293fc348ace4544a56c91e87587b9fcc31a8a772
bfjStart <bfjstart@gmail.com>
2022-06-24 16:01:39 +0800

    新建

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author feng
+ * @create 2022-06-24 15:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("新建文件");
+    }
+}
